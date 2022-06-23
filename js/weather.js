5e317f87da3eb408e1e2c1d67c597daa606da213
zelas2xerath <489650983@qq.com>
2022-06-23 21:36:23 +0800

    Site updated: 2022-06-23 21:36:00

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "00FF00",
+    "tmpSize": "18",
+    "cityColor": "6AF5F5",
+    "citySize": "16",
+    "aqiColor": "7A52DE",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "1",
+    "language": "auto",
+    "borderRadius": "1",
+    "fixed": "true",
+    "vertical": "center",
+    "horizontal": "center",
+    "left": "150",
+    "top": "8",
+    "key": "c38a07a562f54aa4b9a4e227c2bb5aff"
+  }
+}
